4e62e84df4d0e0c59a77b4de81f7aa3fcacf4094
C M <qtekfun@gmail.com>
2025-08-20 12:32:27 +0000

    we have basically the same. The right url is https://qtekfun.com/newWeb/

--- Users/I554734/Documents/code/studio-projects/NextN/next.config.ts
+++ Users/I554734/Documents/code/studio-projects/NextN/next.config.ts
@@ -1,25 +1,16 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   basePath: '/newWeb',
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
